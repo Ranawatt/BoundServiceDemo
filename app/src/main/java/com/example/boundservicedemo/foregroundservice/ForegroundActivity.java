e50754c849ea8812cec3d4b86ad827b6136b178e
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-07-04 19:44:27 +0530

    Working on Foreground Service

---
+++ app/src/main/java/com/example/boundservicedemo/foregroundservice/ForegroundActivity.java
@@ -0,0 +1,37 @@
+package com.example.boundservicedemo.foregroundservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.boundservicedemo.R;
+import com.example.boundservicedemo.foregroundservice.newservice.ExampleService;
+
+public class ForegroundActivity extends AppCompatActivity {
+
+    private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foreground);
+        editText = findViewById(R.id.etTextInput);
+    }
+
+    public void startService(View view) {
+        String input = editText.getText().toString();
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        serviceIntent.putExtra("inputExtra",input);
+//        startService(serviceIntent);
+        ContextCompat.startForegroundService(this,serviceIntent);
+
+    }
+
+    public void stopService(View view) {
+        Intent  serviceIntent = new Intent(this, ExampleService.class);
+        stopService(serviceIntent);
+    }
+}
---
+++ app/src/main/java/com/example/boundservicedemo/foregroundservice/newservice/ExampleService.java
@@ -0,0 +1,56 @@
+package com.example.boundservicedemo.foregroundservice.newservice;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import com.example.boundservicedemo.R;
+import com.example.boundservicedemo.foregroundservice.ForegroundActivity;
+
+import static com.example.boundservicedemo.foregroundservice.App.CHANNEL_ID;
+
+public class ExampleService extends Service {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+//        We are commenting below return statement bcoz we want to return something else
+        super.onStartCommand(intent, flags, startId);
+        String input = intent.getStringExtra("inputExtra");
+        Intent notificationIntent = new Intent(this, ForegroundActivity.class);
+        PendingIntent pendingIntent  = PendingIntent.getActivity(this,0,notificationIntent,0);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Example Service")
+                .setContentText(input)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentIntent(pendingIntent)
+                .build();
+        startForeground(1, notification);
+        Log.d("ExampleService", "Service Started");
+        stopSelf();
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+}
