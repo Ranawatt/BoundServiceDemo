fb0c8cf800aa6b1fb601c5506493877339adbd61
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-07-05 09:02:39 +0530

    Created ServicejobIntentActivity.java

--- app/src/main/java/com/example/boundservicedemo/jobintentservice/ServicejobIntentActivity.java
+++ app/src/main/java/com/example/boundservicedemo/jobintentservice/ServicejobIntentActivity.java
@@ -1,7 +1,6 @@
-package com.example.boundservicedemo.intentservice;
+package com.example.boundservicedemo.jobintentservice;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,23 +9,20 @@ import android.widget.EditText;
 
 import com.example.boundservicedemo.R;
 
-public class ServiceIntentActivity extends AppCompatActivity {
+public class ServicejobIntentActivity extends AppCompatActivity {
 
     private EditText editTextInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_service_intent);
+        setContentView(R.layout.activity_servicejob_intent);
         editTextInput = findViewById(R.id.etTextInput);
     }
 
     public void startService(View view) {
         String input = editTextInput.getText().toString();
-        Intent serviceIntent = new Intent(this, ExampleIntentService.class);
-        serviceIntent.putExtra("inputExtra", input);
-//        startService(serviceIntent);
-        ContextCompat.startForegroundService(this,serviceIntent);
+        Intent serviceIntent = new Intent(this,ExampleJobIntentService.class);
+        serviceIntent.putExtra("inputExtra",input);
+        startService(serviceIntent);
     }
-
-//
 }
