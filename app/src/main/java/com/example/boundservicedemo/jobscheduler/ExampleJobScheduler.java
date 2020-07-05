1a1ffdb9103512327101b7375f0294ccbbb1c8ca
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-07-05 23:30:58 +0530

    Created ExampleJobScheduler.java to schedule jobs

--- app/src/main/java/com/example/boundservicedemo/jobscheduler/ExampleJobScheduler.java
+++ app/src/main/java/com/example/boundservicedemo/jobscheduler/ExampleJobScheduler.java
@@ -2,15 +2,44 @@ package com.example.boundservicedemo.jobscheduler;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.util.Log;
 
 class ExampleJobScheduler extends JobService {
+    private static final String TAG = "ExampleJobScheduler";
+    private boolean jobCancelled = false;
     @Override
     public boolean onStartJob(JobParameters params) {
-        return false;
+        Log.d(TAG,"Job Started") ;
+        doBackgroundWork(params);
+        return true;
+    }
+
+    private void doBackgroundWork(final JobParameters params) {
+        new Thread(new Runnable(){
+
+            @Override
+            public void run() {
+                for(int i=0;i<10;i++){
+                    Log.d(TAG, "RUN: "+i);
+                    if (jobCancelled){
+                        return;
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Log.d(TAG,"Job Finished");
+                jobFinished(params,false);
+            }
+        }).start();
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
-        return false;
+        Log.d(TAG,"Job Cancelled befor completion");
+        jobCancelled = true;
+        return true;
     }
 }
--- app/src/main/java/com/example/boundservicedemo/jobscheduler/SchedulerJobActivity.java
+++ app/src/main/java/com/example/boundservicedemo/jobscheduler/SchedulerJobActivity.java
@@ -2,13 +2,18 @@ package com.example.boundservicedemo.jobscheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.boundservicedemo.R;
 
 public class SchedulerJobActivity extends AppCompatActivity {
 
+    public static final String TAG = "SchedulerJobActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +21,24 @@ public class SchedulerJobActivity extends AppCompatActivity {
     }
 
     public void scheduleJob(View view) {
+        ComponentName componentName = new ComponentName(this,ExampleJobScheduler.class);
+        JobInfo jobInfo = new JobInfo.Builder(123,componentName)
+                .setRequiresCharging(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setPersisted(true)
+                .setPeriodic(15*60*1000).build();
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        int resultCode = scheduler.schedule(jobInfo);
+        if (resultCode == JobScheduler.RESULT_SUCCESS){
+            Log.d(TAG,"Job Scheduled");
+        }else{
+            Log.d(TAG, "Job scheduling Failed");
+        }
     }
 
     public void cancelJob(View view) {
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        scheduler.cancel(123);
+        Log.d(TAG, "Job Cancelled");
     }
 }
