0eb569ce3091595f942f3707bc9b24d9ba3c01a0
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-07-05 20:32:40 +0530

    Working on JobSchedule for Network Operation

---
+++ app/src/main/java/com/example/boundservicedemo/jobscheduler/SchedulerJobActivity.java
@@ -0,0 +1,23 @@
+package com.example.boundservicedemo.jobscheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.boundservicedemo.R;
+
+public class SchedulerJobActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedulerjob);
+    }
+
+    public void scheduleJob(View view) {
+    }
+
+    public void cancelJob(View view) {
+    }
+}
