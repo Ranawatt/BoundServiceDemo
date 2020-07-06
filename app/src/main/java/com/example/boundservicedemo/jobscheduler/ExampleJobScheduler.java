68d350790e43d29894d4390023851b744326a5b5
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-07-06 19:38:42 +0530

    Completed JobScheduler feature

--- app/src/main/java/com/example/boundservicedemo/jobscheduler/ExampleJobScheduler.java
+++ app/src/main/java/com/example/boundservicedemo/jobscheduler/ExampleJobScheduler.java
@@ -4,7 +4,7 @@ import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.util.Log;
 
-class ExampleJobScheduler extends JobService {
+public class ExampleJobScheduler extends JobService {
     private static final String TAG = "ExampleJobScheduler";
     private boolean jobCancelled = false;
     @Override
